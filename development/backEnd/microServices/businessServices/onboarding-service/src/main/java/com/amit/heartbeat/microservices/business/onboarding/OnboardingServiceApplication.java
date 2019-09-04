f8eb96e28e8b7bea68b9a6678df3f0b179bb610d
Amit Bishwas <abishwas@manh.com>
2019-09-04 20:24:42 +0530

    eureka server working

--- development/backEnd/microServices/businessServices/onboarding-service/src/main/java/com/amit/heartbeat/microservices/business/onboarding/OnboardingServiceApplication.java
+++ development/backEnd/microServices/businessServices/onboarding-service/src/main/java/com/amit/heartbeat/microservices/business/onboarding/OnboardingServiceApplication.java
@@ -2,6 +2,7 @@ package com.amit.heartbeat.microservices.business.onboarding;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -10,6 +11,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 @EnableResourceServer
 @EnableOAuth2Client
 @EnableFeignClients
+@EnableDiscoveryClient
 public class OnboardingServiceApplication {
 
     public static void main(String[] args) {
