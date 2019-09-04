8a438dc3d11c081dd2831cce217e5857e2c4414d
Amit Bishwas <abishwas@manh.com>
2019-09-05 00:31:20 +0530

    added config server

--- development/backEnd/microServices/infrastructureServices/config-service/src/test/java/com/amit/heartbeat/microservices/infrastructure/configservice/ConfigServiceApplicationTests.java
+++ development/backEnd/microServices/infrastructureServices/config-service/src/test/java/com/amit/heartbeat/microservices/infrastructure/configservice/ConfigServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.amit.eurekaservice;
+package com.amit.heartbeat.microservices.infrastructure.configservice;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class EurekaServiceApplicationTests {
+public class ConfigServiceApplicationTests {
 
     @Test
     public void contextLoads() {
--- development/backEnd/microServices/infrastructureServices/eureka-service/src/main/java/com/amit/heartbeat/microservices/infrastructure/eurekaservice/EurekaServiceApplication.java
+++ development/backEnd/microServices/infrastructureServices/eureka-service/src/main/java/com/amit/heartbeat/microservices/infrastructure/eurekaservice/EurekaServiceApplication.java
@@ -1,4 +1,4 @@
-package com.amit.eurekaservice;
+package com.amit.heartbeat.microservices.infrastructure.eurekaservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
