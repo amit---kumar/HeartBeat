256549738c0d7336286bb983d304f8e50657329d
Amit Bishwas <abishwas@manh.com>
2019-09-06 12:57:44 +0530

    zuul loadbalaced testing

--- development/backEnd/microServices/businessServices/onboarding-service/src/main/java/com/amit/heartbeat/microservices/business/onboarding/OnboardingServiceApplication.java
+++ development/backEnd/microServices/businessServices/onboarding-service/src/main/java/com/amit/heartbeat/microservices/business/onboarding/OnboardingServiceApplication.java
@@ -3,9 +3,13 @@ package com.amit.heartbeat.microservices.business.onboarding;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableResourceServer
@@ -19,14 +23,14 @@ public class OnboardingServiceApplication {
     }
 
 
-/*    @Configuration
+    @Configuration
     class Config
     {
-        @LoadBalanced
+        //@LoadBalanced
         @Bean
         public RestTemplate restTemplate()
         {
             return new RestTemplate();
         }
-    }*/
+    }
 }
--- development/backEnd/microServices/businessServices/onboarding-service/src/main/java/com/amit/heartbeat/microservices/business/onboarding/controller/GreetController.java
+++ development/backEnd/microServices/businessServices/onboarding-service/src/main/java/com/amit/heartbeat/microservices/business/onboarding/controller/GreetController.java
@@ -1,6 +1,10 @@
 package com.amit.heartbeat.microservices.business.onboarding.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,19 +14,27 @@ import org.springframework.web.client.RestTemplate;
 @RequestMapping(value = "/greet")
 public class GreetController {
 
-//    @Autowired
-//    private RestTemplate restTemplate;
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private Environment env;
 
     @RequestMapping(value = "/formal", method = RequestMethod.GET)
     public String formal()
     {
-        return "Hi ";
+        return "Hi " + env.getProperty("local.server.port");
     }
 
-//    @RequestMapping(value = "/formal/loadbalanced", method = RequestMethod.GET)
-//    public String formalLoadBalanced()
-//    {
-//        String url =  "http://SEARCH/profile/default";
-//        return "Hi " + restTemplate.getForObject(url,String.class);
-//    }
+    @RequestMapping(value = "/formal/search", method = RequestMethod.GET)
+    public String formalLoadBalanced(@RequestHeader("Authorization") String authorization)
+    {
+        org.springframework.http.HttpHeaders httpHeaders = new org.springframework.http.HttpHeaders();
+        String url =  "http://localhost:9180/search/profile/default";
+        String headerName = "Authorization";
+        httpHeaders.add(headerName, authorization);
+        httpHeaders.add("Content-Type", "application/json");
+        HttpEntity<String> requestEntity = new HttpEntity<>("Headers", httpHeaders);
+        return "Hi " + restTemplate.exchange(url, HttpMethod.GET, requestEntity, String.class).getBody();
+    }
 }
