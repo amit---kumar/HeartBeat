d412c17e32ab1fb0f3241663578e4848116ca928
Amit Bishwas <abishwas@manh.com>
2019-09-06 06:35:44 +0530

    search service configured with ouath zuul

--- development/backEnd/microServices/businessServices/search-service/src/main/java/com/amit/heartbeat/microservices/business/searchservice/SearchServiceApplication.java
+++ development/backEnd/microServices/businessServices/search-service/src/main/java/com/amit/heartbeat/microservices/business/searchservice/SearchServiceApplication.java
@@ -3,11 +3,15 @@ package com.amit.heartbeat.microservices.business.searchservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableResourceServer
+@EnableOAuth2Client
+@EnableFeignClients
 public class SearchServiceApplication {
 
     public static void main(String[] args) {
